6ee3a72e82e746cf4cd77179803e9fa856e5f95e
L-alam <lalam@bu.edu>
2025-09-06 17:55:34 -0400

    expo go renders ios build

--- amongyall/lib/supabase.ts
+++ amongyall/lib/supabase.ts
@@ -5,7 +5,7 @@ import { createClient, processLock } from '@supabase/supabase-js'
 
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 
 if (!supabaseUrl || !supabaseAnonKey) {
